9bb5ee1aa4fad28049205909e7050cae96a8f17c
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-12-14 12:25:39 +0200

    New ProductCategoriesController with List action

---
+++ WebEShop/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebEShop.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        // GET: ProductCategories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult List()
+        {
+            List<string> listOfProductCategories = new List<string>();
+            listOfProductCategories.Add(
+                new { Id = 1, Title = "Pen", Description = "Pen Description" }.ToString());
+            listOfProductCategories.Add(
+                new { Id = 2, Title = "Pencil", Description = "Pencil Description" }.ToString());
+            ViewBag.ProductCategories = listOfProductCategories;
+            return View();
+        }
+    }
+}
