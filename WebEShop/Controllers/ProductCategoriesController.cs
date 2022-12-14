edf50550edc89df67a87355e51170f9b2a5b47fe
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-12-14 18:27:24 +0200

    Updated ProductCategories

--- WebEShop/Controllers/ProductCategoriesController.cs
+++ WebEShop/Controllers/ProductCategoriesController.cs
@@ -3,24 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebEShop.Data;
+using WebEShop.Models;
 
 namespace WebEShop.Controllers
 {
     public class ProductCategoriesController : Controller
     {
+        private WebEShopDBContext context;
+
+        public ProductCategoriesController()
+        {
+            context = new WebEShopDBContext();
+        }
         // GET: ProductCategories
         public ActionResult Index()
         {
+            context.ProductCategories.Add(new ProductCategory()
+            {
+                Title = "Pen",
+                Description = "Pen Description",
+                Products = new List<CustomerProduct>()
+                {
+                    new CustomerProduct()
+                    {
+                        Title = "Red Pen",
+                        Description = "Red Pen Description",
+                        Price = 10.45
+                    },
+                    new CustomerProduct()
+                    {
+                        Title = "Blue Pen",
+                        Description = "Blue Pen Description",
+                        Price = 10.45
+                    }
+                }
+            });
+            context.SaveChanges();
             return View();
         }
 
         public ViewResult List()
         {
-            List<string> listOfProductCategories = new List<string>();
-            listOfProductCategories.Add(
-                new { Id = 1, Title = "Pen", Description = "Pen Description" }.ToString());
-            listOfProductCategories.Add(
-                new { Id = 2, Title = "Pencil", Description = "Pencil Description" }.ToString());
+            var listOfProductCategories = context.ProductCategories.ToList();
+            int idOfFirstProductOnCat0 = listOfProductCategories[0].Products.ToList()[0].Id;
+            
             ViewBag.ProductCategories = listOfProductCategories;
             return View();
         }
