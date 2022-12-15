b98177ceac90c2bb78b5050d94c75ff0d1cf55ca
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-12-15 11:53:50 +0200

    KatiController with custom attribure routing in ASP.NET MVC

---
+++ WebEShop/Controllers/KatiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebEShop.Controllers
+{
+    public class KatiController : Controller
+    {
+        // GET: KatiController
+        [Route("Koukou")]
+        [Route("Koukou/Index")]
+        public ActionResult Index()
+        {
+            int i = 10;
+            string s = $@"C:\temp\{i}";
+            string s1 = @"\d+"; // regular expression
+            return View();
+        }
+    }
+}
